e685dbcd563e99847a4ff6c3171a0cd92cfb8a9f
max <poldersen@yandex.com>
2022-11-25 14:31:42 +0000

    augmentations changes

--- ClickSEG/models/hrnet18_cracks.py
+++ ClickSEG/models/hrnet18_cracks.py
@@ -10,10 +10,10 @@ def main(cfg):
 
 def init_model(cfg):
     model_cfg = edict()
-    model_cfg.crop_size = (320, 320)
+    model_cfg.crop_size = (256, 256)
     model_cfg.num_max_points = 24
 
-    model = HRNetModel(width=18, pipeline_version = 's1', small=False, ocr_width=64, with_aux_output=True, use_leaky_relu=True,
+    model = HRNetModel(width=18, pipeline_version = 's2', small=False, ocr_width=64, with_aux_output=True, use_leaky_relu=True,
                        use_rgb_conv=False, use_disks=True, norm_radius=5, with_prev_mask=True)
 
     model.to(cfg.device)
@@ -39,21 +39,21 @@ def train(model, cfg, model_cfg):
     loss_cfg.trimap_loss_weight = 1.0
 
     train_augmentator = Compose([
-        UniformRandomResize(scale_range=(0.75, 1.25)),
-        Flip(),
-        RandomRotate90(),
-        ShiftScaleRotate(shift_limit=0.03, scale_limit=0,
-                         rotate_limit=(-3, 3), border_mode=0, p=0.75),
+        ShiftScaleRotate(shift_limit=0.1, scale_limit=0.0, rotate_limit=(-45, 45), border_mode=2, p=0.75),
+    #     UniformRandomResize(scale_range=(0.75, 1.25)),
+        RandomResizedCrop(*crop_size, scale=(0.3,1.0), p=0.75),
+        Resize(*crop_size),
+        Flip(p=0.75),
+        RandomRotate90(p=0.75),
         PadIfNeeded(min_height=crop_size[0], min_width=crop_size[1], border_mode=0),
-        RandomCrop(*crop_size),
-        RandomBrightnessContrast(brightness_limit=(-0.25, 0.25), contrast_limit=(-0.15, 0.4), p=0.75),
+    #     RandomCrop(*crop_size),
+        RandomBrightnessContrast(brightness_limit=(-0.25, 0.25), contrast_limit=(-0.2, 0.4), p=0.75),
         RGBShift(r_shift_limit=10, g_shift_limit=10, b_shift_limit=10, p=0.75)
     ], p=1.0)
 
     val_augmentator = Compose([
-        UniformRandomResize(scale_range=(0.75, 1.25)),
+        Resize(*crop_size),
         PadIfNeeded(min_height=crop_size[0], min_width=crop_size[1], border_mode=0),
-        RandomCrop(*crop_size)
     ], p=1.0)
 
     points_sampler = MultiPointSampler(model_cfg.num_max_points, prob_gamma=0.80,
